9f64099ff02cc5934387a72e676781fa77853bcf
fujunjie <fujunjie1@qq.com>
2026-02-01 05:25:21 +0000

    add benchmark tools

---
+++ src/base/report/report_client.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+extern "C" {
+
+    bool report(
+        const char* table_name,
+        const char* unique_id,
+        const char* metric_name,
+        double metric_value
+    );
+}
